5043148930d9447def8cc78dd93575bd6a08e3d2
Gurmeet Kaur <131999481+kgurmeet@users.noreply.github.com>
2023-08-17 09:21:39 +0530

    Create pattern3.java

---
+++ pattern3.java
@@ -0,0 +1,17 @@
+import java.util.*;
+public class pattern2 {
+    public static void main(String[] args) {
+        int k=1,y=1;
+        for(k=1;k<=6;k++)
+        {
+            for(y=1;y<=6;y++)
+            {
+                System.out.print(k);
+            }
+            System.out.println();
+        }
+    }
+    
+}
+
+
